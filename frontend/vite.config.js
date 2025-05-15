b4dd692fc09e6de84555a1e9304fbe95b171ef0f
Jasdev-Singh <84892938+Jasdev-Singh@users.noreply.github.com>
2025-05-16 00:44:45 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins:[react()],
     server:{
-        port:process.env.PORT || 3000,
-        host:true
+        port:process.env.PORT || 2992,
+        host:'0.0.0.0',
     }
 });
