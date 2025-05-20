1fc85faa6f68e5c35e28afdbd0ab07a557a4572e
Jasdev-Singh <84892938+Jasdev-Singh@users.noreply.github.com>
2025-05-20 23:04:03 +0530

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthContextProvider } from './context/authContext';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
