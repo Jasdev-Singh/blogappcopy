639496471899d064bee46997fa7ab0ef22a8d910
Jasdev-Singh <84892938+Jasdev-Singh@users.noreply.github.com>
2025-05-20 21:35:35 +0530

    Update Menu.jsx

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-
+import { Link } from 'react-router-dom';
+import { backendurl } from '../context/authContext';
 const Menu = ({cat}) => {
 
    const [posts,setPosts] = useState([]);
@@ -8,7 +9,7 @@ const Menu = ({cat}) => {
   useEffect(()=>{
     const fetchdata = async () =>{
       try{
-        const res = await axios.get(`http://localhost:8800/api/posts/?cat=${cat}`)
+        const res = await axios.get(`${backendurl}/api/posts/?cat=${cat}`)
         console.log(res.data)
         setPosts(res.data);
       }catch(err)
@@ -25,9 +26,10 @@ const Menu = ({cat}) => {
         <h1>Other posts you may like</h1>
         {posts.map(post=>(
             <div className='post' key={post.id}>
-            <img src={post.img} alt="" />
+            <img src={`../upload/${post.img}`} alt="" />
             <h2>{post.title}</h2>
-            <button>Read More</button>
+            <Link className='link' to={`/post/${post.id}`}>
+            <button>Read More</button></Link>
 
             </div>
         ))}
